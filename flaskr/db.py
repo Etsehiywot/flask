d33ae5e3552c7d7ca45991c70fe793efd905f7f2
etsehiywot <etsehiywotdb@gmail.com>
2025-02-21 09:49:58 -0800

    Add deployment configuration

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,9 +1,7 @@
 import sqlite3
-from datetime import datetime
-
 import click
 from flask import current_app, g
-
+from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
@@ -15,29 +13,25 @@ def get_db():
 
     return g.db
 
-
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
 @click.command('init-db')
+@with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
 
-
-sqlite3.register_converter(
-    "timestamp", lambda v: datetime.fromisoformat(v.decode())
-)
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
